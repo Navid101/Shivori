3a326bba7a079041a614716f579bd8784953e7b0
Navid <navid.anjum@northsouth.edu>
2022-01-07 19:17:16 +0600

    font and desc

--- pages/[productCategory]/[product].js
+++ pages/[productCategory]/[product].js
@@ -167,7 +167,8 @@ const product = ({products}) => {
                         <h3>{item.name}</h3>
                         <h3>TK: {item.price}</h3>
                         <h3>Fabric: {item.subCategory}</h3>
-                        <h3>Desc: {item.desc}</h3>
+                        <h3>SKU: {item.sku}</h3>
+                        <h3>{item.desc}</h3>
                         {singleProduct.map((item)=>{
                             if(item.size){
                                 return <FilterSize sizes={item.size} key={item.sku}></FilterSize>
