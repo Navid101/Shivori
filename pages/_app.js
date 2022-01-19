027c66de6c5accec8b10b85555b858b81be75fc0
Navid <navid.anjum@northsouth.edu>
2022-01-19 15:08:33 +0600

    facebook chat

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,17 @@ import Footer from '../components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from 'react-redux'
 import store from "./../redux/store"
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
     <Navbar></Navbar>
     <Component {...pageProps} />
+    <MessengerCustomerChat
+    pageId="100398064943718"
+    appId="1660785550761634"
+    />
     <Footer></Footer>
     </Provider>
     )
